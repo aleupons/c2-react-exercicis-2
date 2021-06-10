31949954a106567ed0220099aa82db6fdbebbd74
aleupons <apahoquei@gmail.com>
2021-06-10 18:14:38 +0200

    Funcionalitats botons: incrementar, decrementar i límits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return;
+  const [valor, setValor] = useState(0);
+  const incrementa = () => {
+    if (valor < 10) {
+      setValor(valor + 1);
+    }
+  };
+  const decrementa = () => {
+    if (valor > 0) {
+      setValor(valor - 1);
+    }
+  };
+  return (
+    <div className="contenidor">
+      <button onClick={decrementa}>-</button>
+      <input type="number" value={valor} readOnly></input>
+      <button onClick={incrementa}>+</button>
+    </div>
+  );
 }
 
 export default App;
